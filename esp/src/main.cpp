f6a218d7e587bd5110961021575af2feec3d09b7
jakrol <kozak10555@gmail.com>
2026-03-18 22:18:19 +0100

    added esp website hosting:

--- esp/src/main.cpp
+++ esp/src/main.cpp
@@ -1,18 +1,73 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <ESPAsyncWebServer.h>
+#include <LittleFS.h>
+#include <DNSServer.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const char* ssid = "ESP";
+const char* password = NULL; 
+
+AsyncWebServer server(80);
+DNSServer dnsServer;
+
+// DANE TESTOWE 
+String soil = "450";     
+String water = "15";     
+String temp = "24.5";    
+String hum = "45.0";     
+String press = "1013.2"; 
+String pump = "OFF";     
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+
+  // Getting website from flash
+  if (!LittleFS.begin()) {
+    Serial.println("Błąd montowania LittleFS!");
+  }
+
+  // Access point init
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ssid);
+
+  IPAddress IP = WiFi.softAPIP();
+  Serial.print("Access Point uruchomiony! Nazwa: ");
+  Serial.println(ssid);
+  Serial.print("Adres IP: ");
+  Serial.println(IP);
+
+  // Route dns requests to website
+  dnsServer.start(53, "*", IP);
+
+  // Frontend endpoint
+  server.on("/api/data", HTTP_GET, [](AsyncWebServerRequest *request){
+    String json = "{";
+    json += "\"soil\":" + soil + ",";
+    json += "\"water\":" + water + ",";
+    json += "\"temp\":" + temp + ",";
+    json += "\"hum\":" + hum + ",";
+    json += "\"press\":" + press + ",";
+    json += "\"pump\":\"" + pump + "\"";
+    json += "}";
+    request->send(200, "application/json", json);
+  });
+
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(LittleFS, "/index.html", "text/html");
+  });
+
+  server.onNotFound([](AsyncWebServerRequest *request){
+    request->redirect("/");
+  });
+
+  // Getting rest of the files
+  // server.serveStatic("/", LittleFS, "/");
+
+  server.begin();
+  Serial.println("Serwer WWW uruchomiony.");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // For captive portal, dns needs to loop
+  dnsServer.processNextRequest();
 }
