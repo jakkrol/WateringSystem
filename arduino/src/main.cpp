0d37e095b2dfc44a47e92aa97f473b81fe74f475
jakrol <kozak10555@gmail.com>
2026-03-30 12:00:36 +0200

    system fully working I guess

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,30 +1,32 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
-#define BME280_ADDRESS 0x76
-Adafruit_BME280 bme;
+SoftwareSerial espSerial(8, 9); // RX, TX
+
+Adafruit_BME280 bme; 
 
 const int MOSFET_PIN = 3; 
 const int SENSOR_PIN = A0;
-
 const int HCR_trig = 4; 
 const int HCR_echo = 5; 
 
+
 const int DRY_THRESHOLD = 700;  
 const int WET_THRESHOLD = 600;  
-
 const int WATER_MIN_DISTANCE = 20; 
 
 void setup() {
-  Serial.begin(9600);
-
-  // BME280 
-  // if (!bme.begin(BME280_ADDRESS )) {
-  //   Serial.println("Can't find BME280!");
-  //   while(1) delay(10);
-  // }
+  Serial.begin(115200);
+  // Serial do ESP32 (SoftwareSerial najlepiej działa na 9600)
+  espSerial.begin(9600);
+  
+  // BME280 init
+  if (!bme.begin(0x76)) {
+    Serial.println("Błąd: Nie znaleziono BME280 (sprawdź piny A4/A5)!");
+  }
 
   pinMode(MOSFET_PIN, OUTPUT);
   digitalWrite(MOSFET_PIN, LOW); 
@@ -32,11 +34,11 @@ void setup() {
   pinMode(HCR_trig, OUTPUT);
   pinMode(HCR_echo, INPUT);
 
-  Serial.println("System nawadniania: Test czujników i pompy.");
+  Serial.println("System Arduino uruchomiony.");
 }
 
 void loop() {
-  int sensorValue = analogRead(SENSOR_PIN);
+  int soilVal = analogRead(SENSOR_PIN);
 
   digitalWrite(HCR_trig, LOW);
   delayMicroseconds(2);
@@ -44,38 +46,39 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(HCR_trig, LOW);
   
-  long duration = pulseIn(HCR_echo, HIGH, 30000); // Timeout 30ms
-  int distance = duration * 0.034 / 2;
+  long duration = pulseIn(HCR_echo, HIGH, 30000);
+  int distance = (duration > 0) ? (duration * 0.034 / 2) : 999;
 
-  Serial.print("Wilgotność gleby: ");
-  Serial.print(sensorValue);
-  Serial.print(" | Dystans: ");
-  Serial.print(distance);
-  Serial.println(" cm");
+  float t = bme.readTemperature();
+  float h = bme.readHumidity();
+  float p = bme.readPressure() / 100.0F;
 
-
-  // --- (MOSFET) ---
-  if (sensorValue > DRY_THRESHOLD) {
-    if (distance > 0 && distance < WATER_MIN_DISTANCE) {
-      Serial.println(">>> Sucho i jest woda");
+  bool pumpState = false;
+  
+  if (soilVal > DRY_THRESHOLD && distance < WATER_MIN_DISTANCE && distance > 0) {
       digitalWrite(MOSFET_PIN, HIGH);
-    } else {
-      Serial.println("!!! Sucho, ale brak wody");
-      digitalWrite(MOSFET_PIN, LOW);
-    }
+      pumpState = true;
   } 
-  else if (sensorValue < WET_THRESHOLD) {
-    Serial.println("Wilgotno");
-    digitalWrite(MOSFET_PIN, LOW);
+  else if (soilVal < WET_THRESHOLD) {
+      digitalWrite(MOSFET_PIN, LOW);
+      pumpState = false;
+  }
+  else if (distance >= WATER_MIN_DISTANCE) {
+      digitalWrite(MOSFET_PIN, LOW);
+      pumpState = false;
   }
 
+  espSerial.print(soilVal);   espSerial.print(",");
+  espSerial.print(distance);  espSerial.print(",");
+  espSerial.print(t);         espSerial.print(",");
+  espSerial.print(h);         espSerial.print(",");
+  espSerial.print(p);         espSerial.print(",");
+  espSerial.println(pumpState ? "1" : "0"); 
 
-  // Serial.print("Temperature in degC = ");
-  // Serial.println(bme.readTemperature());
-  // Serial.print("Pressure in hPa     = ");
-  // Serial.println(bme.readPressure() / 100.0F);
-  // Serial.print("Humidity in %RH     = ");
-  // Serial.println(bme.readHumidity());
+  Serial.print("Soil: "); Serial.print(soilVal);
+  Serial.print(" | Dist: "); Serial.print(distance);
+  Serial.print(" | Temp: "); Serial.print(t);
+  Serial.print(" | Pump: "); Serial.println(pumpState ? "ON" : "OFF");
 
   delay(2000); 
 }
--- esp/src/main.cpp
+++ esp/src/main.cpp
@@ -4,42 +4,37 @@
 #include <LittleFS.h>
 #include <DNSServer.h>
 
-const char* ssid = "ESP";
+const char* ssid = "System_Nawadniania_ESP";
 const char* password = NULL; 
 
 AsyncWebServer server(80);
 DNSServer dnsServer;
 
-// DANE TESTOWE 
-String soil = "600";     
-String water = "15";     
-String temp = "24.5";    
-String hum = "45.0";     
-String press = "1013.2"; 
+String soil = "0";      
+String water = "0";     
+String temp = "0";      
+String hum = "0";       
+String press = "0";     
 boolean pump = false;     
 
 void setup() {
   Serial.begin(115200);
 
-  // Getting website from flash
+  Serial2.begin(9600, SERIAL_8N1, 19, 18);
+
   if (!LittleFS.begin()) {
     Serial.println("Błąd montowania LittleFS!");
   }
 
-  // Access point init
   WiFi.mode(WIFI_AP);
   WiFi.softAP(ssid);
 
   IPAddress IP = WiFi.softAPIP();
-  Serial.print("Access Point uruchomiony! Nazwa: ");
-  Serial.println(ssid);
-  Serial.print("Adres IP: ");
+  Serial.print("AP uruchomiony. IP: ");
   Serial.println(IP);
 
-  // Route dns requests to website
   dnsServer.start(53, "*", IP);
 
-  // Frontend endpoint
   server.on("/api/data", HTTP_GET, [](AsyncWebServerRequest *request){
     String json = "{";
     json += "\"soil\":" + soil + ",";
@@ -52,30 +47,52 @@ void setup() {
     request->send(200, "application/json", json);
   });
 
+  // Obsługa plików statycznych z pamięci Flash
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send(LittleFS, "/index.html", "text/html");
   });
 
   server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
-  request->send(LittleFS, "/style.css", "text/css");
-});
+    request->send(LittleFS, "/style.css", "text/css");
+  });
 
-server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest *request){
-  request->send(LittleFS, "/script.js", "text/javascript");
-});
+  server.on("/script.js", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(LittleFS, "/script.js", "text/javascript");
+  });
 
+  // Captive Portal - przekierowanie
   server.onNotFound([](AsyncWebServerRequest *request){
     request->redirect("/");
   });
 
-  // Getting rest of the files
-  // server.serveStatic("/", LittleFS, "/");
-
   server.begin();
-  Serial.println("Serwer WWW uruchomiony.");
+  Serial.println("Serwer WWW gotowy.");
 }
 
 void loop() {
-  // For captive portal, dns needs to loop
   dnsServer.processNextRequest();
+
+  if (Serial2.available() > 0) {
+    String rawData = Serial2.readStringUntil('\n');
+    rawData.trim();
+
+    int c1 = rawData.indexOf(',');
+    int c2 = rawData.indexOf(',', c1 + 1);
+    int c3 = rawData.indexOf(',', c2 + 1);
+    int c4 = rawData.indexOf(',', c3 + 1);
+    int c5 = rawData.indexOf(',', c4 + 1);
+
+    if (c5 != -1) {
+      soil = rawData.substring(0, c1);
+      water = rawData.substring(c1 + 1, c2);
+      temp = rawData.substring(c2 + 1, c3);
+      hum = rawData.substring(c3 + 1, c4);
+      press = rawData.substring(c4 + 1, c5);
+      //pump ma być boolean
+      pump = (rawData.substring(c5 + 1) == "1");
+
+      // Debug
+      Serial.println("Odebrano: " + rawData);
+    }
+  }
 }
